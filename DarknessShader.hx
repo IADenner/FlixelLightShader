d57edd43f91cd8225641167dce0903c45d94a542
Isaac Denner <iadenner@gmail.com>
2021-03-03 16:55:46 -0500

    Add files via upload
    Commented shader file for readability

--- DarknessShader.hx
+++ DarknessShader.hx
@@ -18,31 +18,46 @@ class DarknessShader extends FlxShader
 		uniform sampler2D sourceList;
 		void main()
 		{
+			//this is the variable we will be MULTIPLYING our canvas colros by
 			vec4 lightness;
+			
+			//We grab our base value off of the last pixel of our darknessData bitmap. This is set in the last few lines of DarknessData's update function. This will be the color anything not lit will be
+			//multiplied by. 
 			lightness = texture2D(sourceList, vec2(0.995, 0.25));
+			
+			//iterate through the bitmap horizontally.
 			for (float iterator = 0.0; iterator < 127.0; ++iterator)
 			{
-				//get the point on our texture where 
-				vec2 sampleCoord = vec2(iterator/127.0, 0.25);
+				//get the point on our texture where we will sample.
+				vec2 sampleCoord = vec2(iterator / 127.0, 0.25);
+				
+				//this is the xy + brightness/radius data
 				vec4 value = texture2D(sourceList, sampleCoord);
+				
+				//grab just the xy and calculate the distance. 
 				vec2 lightCoord = value.xy;
 				float dist = distance(lightCoord, openfl_TextureCoordv);
 				if (dist < value.z)
 				{
-	vec4 colorValue = texture2D(sourceList, vec2(iterator / 127.0, 0.75));
+					//If you want to create different 'shapes' of lights, here's where you can do it. I wanted my lights to have a squared shape falloff, so I take lightness = (dist/brightness)^2
+					//You could change this up so you have a flat falloff lightness = (dist/brightness), or a super sharp falloff (lightness = 1)
+					vec4 colorValue = texture2D(sourceList, vec2(iterator / 127.0, 0.75));
 					lightness.x += (1.0 - dist * dist / (value.z * value.z)) * colorValue.x;
 					lightness.y += (1.0 - dist * dist / (value.z * value.z)) * colorValue.y;
 					lightness.z += (1.0 - dist*dist / (value.z * value.z)) * colorValue.z;
 				}
 			}
 			
+			
+			//clamp all of our values
 			vec4 thisAlpha;
 			thisAlpha.x = max(0.0, min(1.0, lightness.x));
 			thisAlpha.y = max(0.0, min(1.0, lightness.y));
 			thisAlpha.z = max(0.0, min(1.0, lightness.z));
 			
+			//now grab our canvas and multiply everything! Tada, we're done!
 			vec4 baseColor = texture2D(bitmap, openfl_TextureCoordv);
-			gl_FragColor = vec4(baseColor.x*thisAlpha.x, baseColor.y*thisAlpha.y, baseColor.z*thisAlpha.z, 1.0); //square our blues for blue tinge for now
+			gl_FragColor = vec4(baseColor.x*thisAlpha.x, baseColor.y*thisAlpha.y, baseColor.z*thisAlpha.z, 1.0);
 			
 		}
 		
